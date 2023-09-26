fa7a84c598c03933a03881341f66b1f116cd6cd0
Thom Bruce <thom@thombruce.com>
2023-09-26 03:29:06 +0100

    move player sprite to separate spawn function

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, spawn_player))
         .run();
 }
 
 /// The setup function
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(SpriteBundle {
         texture: asset_server.load("space/ships/playerShip2_blue.png"),
         ..default()
