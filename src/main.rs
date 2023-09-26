7d0a2cc95ba5e3078bc9cc99644b8ea7ce427771
Thom Bruce <thom@thombruce.com>
2023-09-26 03:28:02 +0100

    spawn 2d camera and ship sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+/// The setup function
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("space/ships/playerShip2_blue.png"),
+        ..default()
+    });
 }
