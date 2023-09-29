4121b951b2e8d829f8581e7f993c3e847d037cd7
Thom Bruce <thom@thombruce.com>
2023-09-29 03:29:59 +0100

    add location indicator to HUD and new font

--- src/hud.rs
+++ src/hud.rs
@@ -15,14 +15,14 @@ impl Plugin for HudPlugin {
     }
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn((NodeBundle {
             style: Style {
                 position_type: PositionType::Absolute,
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
-                align_items: AlignItems::FlexStart,
+                align_items: AlignItems::FlexEnd,
                 justify_content: JustifyContent::FlexEnd,
                 flex_direction: FlexDirection::Row,
                 ..default()
@@ -45,8 +45,9 @@ fn setup(mut commands: Commands) {
                     text: Text::from_section(
                         "0",
                         TextStyle {
-                            font_size: 50.0,
-                            color: Color::rgb_u8(0x00, 0xAA, 0xAA),
+                            font: asset_server.load("fonts/kenvector_future.ttf"),
+                            font_size: 25.0,
+                            color: Color::rgb_u8(0xAA, 0xAA, 0x33),
                             ..default()
                         },
                     ),
@@ -54,6 +55,44 @@ fn setup(mut commands: Commands) {
                 },
                 UISpeed {},
             ));
+        });
+
+    commands
+        .spawn((NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::FlexStart,
+                justify_content: JustifyContent::FlexEnd,
+                flex_direction: FlexDirection::Row,
+                ..default()
+            },
+            ..default()
+        },))
+        .with_children(|parent| {
+            parent.spawn((TextBundle {
+                style: Style {
+                    justify_content: JustifyContent::FlexEnd,
+                    margin: UiRect {
+                        left: Val::Px(10.0),
+                        right: Val::Px(10.0),
+                        top: Val::Px(10.0),
+                        bottom: Val::Px(10.0),
+                    },
+                    ..default()
+                },
+                text: Text::from_section(
+                    "In Space",
+                    TextStyle {
+                        font: asset_server.load("fonts/kenvector_future.ttf"),
+                        font_size: 25.0,
+                        color: Color::rgb_u8(0x00, 0xAA, 0xAA),
+                        ..default()
+                    },
+                ),
+                ..default()
+            },));
         });
 }
 
