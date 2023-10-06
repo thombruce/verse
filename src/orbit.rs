b2c9d69995130803199ebcec58b6722a104b6bc5
Thom Bruce <thom@thombruce.com>
2023-10-06 22:57:47 +0100

    decouple planet from parent star

--- src/orbit.rs
+++ src/orbit.rs
@@ -6,25 +6,45 @@ const ORBITAL_PERIOD_SCALING_FACTOR: f32 = 1.0;
 
 #[derive(Component, Clone, Debug)]
 pub struct Orbit {
+    pub parent: Option<Entity>,
     pub semi_major_axis: f32,
     // pub eccentricity: f32,
     // pub argument_of_periapsis: f32,
     // pub initial_mean_anomaly: f32,
 }
 
-/// Really basic circular motion around (0., 0.) or parent entity
+/// Really basic circular motion around a parent body or [0., 0.]
 pub fn orbital_positioning_system(
     game_time: Res<GameTime>,
     mut orbits: Query<(&Orbit, &mut Transform)>,
+    // TODO: Orbiting bodies should be able to orbit other bodies
+    //       This might call for use of a ParamSet,
+    //       except... I also can't borrow the ParamSet more than once.
+    //       Technically, the thing I want below is also in the above,
+    //       so it's a question of how do I borrow from the above twice?
+    //       This is what copy and clone are for. Look into this.
+    entity_query: Query<&Transform, Without<Orbit>>,
 ) {
     for (orbit, mut transform) in orbits.iter_mut() {
-        transform.translation.x = (game_time.elapsed_secs() / orbit.semi_major_axis.sqrt()
-            * ORBITAL_PERIOD_SCALING_FACTOR)
-            .cos()
-            * orbit.semi_major_axis;
-        transform.translation.y = (game_time.elapsed_secs() / orbit.semi_major_axis.sqrt()
-            * ORBITAL_PERIOD_SCALING_FACTOR)
-            .sin()
-            * orbit.semi_major_axis;
+        let mut entity_translation = Vec3::ZERO;
+
+        if let Some(parent) = orbit.parent {
+            let entity_transform = entity_query.get(parent);
+            entity_translation = match entity_transform {
+                Ok(transform) => transform.translation,
+                Err(_) => Vec3::ZERO,
+            };
+        }
+
+        transform.translation.x = entity_translation.x
+            + (game_time.elapsed_secs() / orbit.semi_major_axis.sqrt()
+                * ORBITAL_PERIOD_SCALING_FACTOR)
+                .cos()
+                * orbit.semi_major_axis;
+        transform.translation.y = entity_translation.y
+            + (game_time.elapsed_secs() / orbit.semi_major_axis.sqrt()
+                * ORBITAL_PERIOD_SCALING_FACTOR)
+                .sin()
+                * orbit.semi_major_axis;
     }
 }
--- src/planetary_system.rs
+++ src/planetary_system.rs
@@ -12,7 +12,9 @@ use crate::{
 pub struct PlanetarySystemPlugin;
 impl Plugin for PlanetarySystemPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::GameCreate), setup);
+        // TODO: Having some real trouble with ordering systems
+        app.add_systems(OnExit(AppState::StartMenu), spawn_star);
+        app.add_systems(OnEnter(AppState::GameCreate), spawn_planets);
         app.add_systems(
             Update,
             (animate_sprite, orbital_positioning_system).run_if(in_state(AppState::Active)),
@@ -49,55 +51,64 @@ fn animate_sprite(
     }
 }
 
-fn setup(mut commands: Commands, sprites: Res<SpriteAssets>) {
+fn spawn_star(mut commands: Commands, sprites: Res<SpriteAssets>) {
     // Star
     let star_animation_indices = AnimationIndices {
         first: 0,
         last: 124,
     };
 
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: sprites.star.clone(),
+            sprite: TextureAtlasSprite::new(star_animation_indices.first),
+            transform: Transform::from_scale(Vec3::splat(2.0)),
+            ..default()
+        },
+        star_animation_indices,
+        // TODO: .1 is too fast, .2 is too choppy; needs more animation frames.
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Star {},
+        Indicated {
+            color: Color::ANTIQUE_WHITE,
+        },
+        // Orbit {
+        //     parent: None,
+        //     semi_major_axis: 0.,
+        // },
+        Name::new("Star"),
+    ));
+}
+
+fn spawn_planets(
+    mut commands: Commands,
+    sprites: Res<SpriteAssets>,
+    star_query: Query<Entity, With<Star>>,
+) {
     // Planet
     let planet_animation_indices = AnimationIndices {
         first: 0,
         last: 124,
     };
 
-    commands
-        .spawn((
-            SpriteSheetBundle {
-                texture_atlas: sprites.star.clone(),
-                sprite: TextureAtlasSprite::new(star_animation_indices.first),
-                transform: Transform::from_scale(Vec3::splat(2.0)),
-                ..default()
-            },
-            star_animation_indices,
-            // TODO: .1 is too fast, .2 is too choppy; needs more animation frames.
-            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-            Star {},
-            Indicated {
-                color: Color::ANTIQUE_WHITE,
-            },
-            Name::new("Star"),
-        ))
-        .with_children(|parent| {
-            parent.spawn((
-                SpriteSheetBundle {
-                    texture_atlas: sprites.planet.clone(),
-                    sprite: TextureAtlasSprite::new(planet_animation_indices.first),
-                    transform: Transform::from_scale(Vec3::splat(2.0 / 2.0)), // Divide by parent scale?
-                    ..default()
-                },
-                planet_animation_indices,
-                // TODO: .1 is too fast, .2 is too choppy; needs more animation frames.
-                AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-                Planet {},
-                Indicated {
-                    color: Color::LIME_GREEN,
-                },
-                Orbit {
-                    semi_major_axis: 500.0 / 2.0, // Divide by parent scale?
-                },
-                Name::new("Planet"),
-            ));
-        });
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: sprites.planet.clone(),
+            sprite: TextureAtlasSprite::new(planet_animation_indices.first),
+            transform: Transform::from_scale(Vec3::splat(2.0)), // Divide by parent scale?
+            ..default()
+        },
+        planet_animation_indices,
+        // TODO: .1 is too fast, .2 is too choppy; needs more animation frames.
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        Planet {},
+        Indicated {
+            color: Color::LIME_GREEN,
+        },
+        Orbit {
+            parent: Some(star_query.single()),
+            semi_major_axis: 500.0,
+        },
+        Name::new("Planet"),
+    ));
 }
