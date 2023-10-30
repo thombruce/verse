ce8bc9b5f232ba793edce3fe9a8fc7f3b87173ef
Thom Bruce <thom@thombruce.com>
2023-10-30 18:34:07 +0000

    remove redundant code examples

--- src/common_assets_demo.rs
+++ src/common_assets_demo.rs
@@ -8,24 +8,11 @@ pub struct CommonAssetsDemoPlugin;
 impl Plugin for CommonAssetsDemoPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RonAssetPlugin::<Config>::new(&["config.ron"]))
-            .add_systems(
-                OnEnter(GameState::StartMenu),
-                (spawn_config, spawn_config_again).run_if(in_state(GameState::StartMenu)),
-            );
+            .add_systems(OnEnter(GameState::StartMenu), spawn_config);
     }
 }
 
 fn spawn_config(data: Res<DataAssets>, configs: ResMut<Assets<Config>>) {
-    if let Some(config) = configs.get(&data.config.clone()) {
-        for position in &config.positions {
-            println!("{}", position[0]);
-        }
-    }
-}
-
-// It only works the FIRST time. I think because .remove() removes
-// the data, it cannot be found again by a later system.
-fn spawn_config_again(data: Res<DataAssets>, configs: ResMut<Assets<Config>>) {
     if let Some(config) = configs.get(&data.config.clone()) {
         for position in &config.positions {
             println!("{}", position[0]);
