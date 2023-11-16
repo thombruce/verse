9703b41c80daeeaa0f640eb8b7de4c14452fe882
Thom Bruce <thom@thombruce.com>
2023-11-16 10:14:03 +0000

    bump time scale up to 1 hour per second

--- src/core/resources/game_time.rs
+++ src/core/resources/game_time.rs
@@ -11,6 +11,13 @@ impl Plugin for GameTimePlugin {
 }
 
 pub(crate) fn tick_game_time(time: Res<Time>, mut game_time: ResMut<GameTime>) {
-    game_time.0.tick(20 * time.delta());
     // 1 day = 72 minutes
+    // game_time.0.tick(20 * time.delta());
+
+    // 1 month = 12 minutes
+    game_time.0.tick(60 * 60 * time.delta());
+    // This is about as fast as I would ever want the core game time progression
+    // to be. Planets are noticably moving, but not so fast that it *feels*
+    // too unnatural. It feels suitable for the game as an arcade shooter,
+    // so this is probably where we'll land for v0.1.
 }
