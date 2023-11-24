9800ebd3c4891c44c4a478d8bab617a6c052aaf0
Thom Bruce <thom@thombruce.com>
2023-11-24 01:11:25 +0000

    add background shader borrowed from Kataster

---
+++ assets/shaders/background.wgsl
@@ -0,0 +1,67 @@
+// This shader is inspired by Star Nest by Pablo Roman Andrioli:
+// https://www.shadertoy.com/view/XlfGRj
+
+// Adapted for Verse from https://github.com/BorisBoutillier/Kataster/blob/main/assets/background.wgsl
+
+#import bevy_sprite::mesh2d_vertex_output::VertexOutput
+
+struct BackgroundMaterial {
+    time: f32,
+};
+
+@group(1) @binding(0)
+var<uniform> background: BackgroundMaterial;
+
+const iterations = 17;
+const formuparam = 0.53;
+
+const volsteps = 20;
+const stepsize = 0.1;
+
+const zoom = 0.800;
+const tile = 0.850;
+const speed = 0.003;
+
+const brightness = 0.0015;
+const darkmatter = 0.600;
+const distfading = 0.730;
+const saturation = 0.850;
+
+@fragment
+fn fragment(
+    in: VertexOutput
+) -> @location(0) vec4<f32> {
+    let dir = vec3<f32>(in.uv * zoom, 1.0);
+    let time = background.time * speed + 0.25;
+    var from_ = vec3<f32>(1.0, 0.5, 0.5);
+    from_ = from_ + vec3<f32>(time * 2., time, -2.);
+    
+    // volumetric rendering
+    var s = 0.1;
+    var fade = 1.0;
+    var v = vec3<f32>(0.);
+    for (var r = 0; r < volsteps; r = r + 1) {
+        var p = from_ + s * dir * 0.5;
+        p = abs(vec3<f32>(tile) - (p % vec3<f32>(tile * 2.0)));
+
+        var pa = 0.0;
+        var a = 0.0;
+        for (var i = 0; i < iterations; i = i + 1) {
+            p = abs(p) / dot(p, p) - formuparam; // the magic formula
+            a = a + abs(length(p) - pa); // absolute sum of average change
+            pa = length(p);
+        }
+
+        let dm = max(0.0, darkmatter - a * a * 0.001); // dark matter
+        a = a * a * a; // add contrast
+        if r > 6 {
+            fade = fade * (1. - dm); // dark matter, don't render near
+        }
+        v = v + fade;
+        v = v + vec3<f32>(s, s * s, s * s * s * s) * a * brightness * fade; // coloring based on distance
+        fade = fade * distfading; // distance fading;
+        s = s + stepsize;
+    }
+    v = mix(vec3<f32>(length(v)), v, saturation); // color_adjust
+    return vec4<f32>(v * 0.0006, 1.0);
+}
---
+++ src/world/astronomy/background.rs
@@ -0,0 +1,67 @@
+// Adapted for Verse from https://github.com/BorisBoutillier/Kataster/blob/main/src/background.rs
+
+use bevy::prelude::*;
+use bevy::render::render_resource::{AsBindGroup, ShaderRef};
+use bevy::sprite::Material2dPlugin;
+use bevy::{
+    reflect::TypeUuid,
+    sprite::{Material2d, MaterialMesh2dBundle},
+};
+
+use crate::systems::states::GameState;
+
+pub const ARENA_WIDTH: f32 = 1500.0; // TODO: Change me
+pub const ARENA_HEIGHT: f32 = 750.0; // TODO: Change me
+
+// Plugin that will insert a background at Z = -10.0, use the custom 'Star Nest' shader
+pub struct BackgroundPlugin;
+impl Plugin for BackgroundPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(Material2dPlugin::<BackgroundMaterial>::default())
+            .add_systems(Startup, spawn_background)
+            .add_systems(Update, update_background_time);
+    }
+}
+
+// Spawn a simple stretched quad that will use of backgound shader
+fn spawn_background(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<BackgroundMaterial>>,
+) {
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform {
+            translation: Vec3::new(0.0, -6000.0, 0.0), // TODO: Probably follow player/camera??
+            scale: Vec3::new(ARENA_WIDTH, ARENA_HEIGHT, 1.0),
+            ..default()
+        },
+        material: materials.add(BackgroundMaterial { time: 0.0 }),
+        ..default()
+    });
+}
+
+#[derive(Asset, AsBindGroup, Debug, Clone, TypeUuid, TypePath)]
+#[uuid = "d1776d38-712a-11ec-90d6-0242ac120003"]
+struct BackgroundMaterial {
+    #[uniform(0)]
+    time: f32,
+}
+
+impl Material2d for BackgroundMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/background.wgsl".into()
+    }
+}
+
+fn update_background_time(
+    time: Res<Time>,
+    state: Res<State<GameState>>,
+    mut backgrounds: ResMut<Assets<BackgroundMaterial>>,
+) {
+    if state.get() != &GameState::Paused {
+        for (_, background) in backgrounds.iter_mut() {
+            background.time += time.delta_seconds();
+        }
+    }
+}
